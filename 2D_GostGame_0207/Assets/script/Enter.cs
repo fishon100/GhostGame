17755eb885e744ab2c74e80dcf67dc17829882de
fishon100 <75553274+fishon100@users.noreply.github.com>
2021-03-28 00:28:20 +0800

    腳色控制 點擊物品

---
+++ 2D_GostGame_0207/Assets/script/Enter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enter : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.tag = "Enter";
+    }
+    public void CangeScene()
+    {
+        SceneManager.LoadScene("廊道");
+    }
+
+
+}
---
+++ 2D_GostGame_0207/Assets/script/Player.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour
+{
+    #region 欄位
+    [Header("移動速度"), Range(0, 20)]
+    public float movespeed = 5f;
+
+    //public float h;
+
+    Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator ani;
+
+    public EventTrigger Eve ;
+
+
+    //private SpriteRenderer spr;
+
+    #endregion
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //動畫名稱 = 取得元件<動畫控制器>();
+        ani = GetComponent<Animator>();
+        //圖片名稱 = 取得元件<圖片控制>()
+        //spr = GetComponent<SpriteRenderer>();
+
+         Eve = GetComponent<EventTrigger>();
+
+        
+
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        ani.SetFloat("Horizontal", movement.x);
+        ani.SetFloat("Vertical", movement.y);
+        ani.SetFloat("Speed", movement.sqrMagnitude);
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+    }
+
+
+    #region 方法
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Enter")
+        {
+            coll.gameObject.transform.GetComponent<Enter>().CangeScene();
+            coll.gameObject.transform.GetComponent<Enter1>().CangeScene();
+        }
+
+        if (coll.gameObject.name == "櫃台")
+        {
+            
+        }
+    }
+
+    
+
+
+
+
+
+    #endregion
+}
